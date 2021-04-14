6ebcc4ca25a22547fb3be61aea262d804c93b72c
r0nuh <cs.szilard@gmail.com>
2021-04-14 23:29:30 +0200

    add project start

---
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-list-edit.component.html',
+  styleUrls: ['./shopping-list-edit.component.css']
+})
+export class ShoppingListEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
